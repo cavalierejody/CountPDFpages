4bd17cd4265f199884d77f1bc8844e8c094d43b6
cavalierejody <cavalierejody@yahoo.it>
2015-05-21 11:00:40 +0200

    aggiunta funzionalità per contrare anche il numero di file pdf

--- contaPagine.py
+++ contaPagine.py
@@ -17,18 +17,21 @@ while choice not in set(['y','n']):
 lookAtSubFolders = False
 if choice == 'y': lookAtSubFolders = True
 
+nPDFFiles=0
 if lookAtSubFolders:
     for root, dirs, files in os.walk("./"):
 	for file in files:
             if file.endswith(".pdf"):
+		nPDFFiles+=1
 		filePath=os.path.join(root, file)
 		filePdf=PdfFileReader(open(filePath,'rb'))
 		pdfDictionary[filePath]=filePdf.getNumPages()
 else:
     for file in os.listdir("./"):
 	if file.endswith(".pdf"):
-		filePdf=PdfFileReader(open(file,'rb'))
-		pdfDictionary[file]=filePdf.getNumPages()
+	    nPDFFiles+=1
+	    filePdf=PdfFileReader(open(file,'rb'))
+	    pdfDictionary[file]=filePdf.getNumPages()
 
 print
 totPag = 0
@@ -37,4 +40,5 @@ for entry in pdfDictionary:
     totPag+=pdfDictionary[entry]
 
 print
+print "Number of PDFs:", nPDFFiles
 print "Total pages:", totPag
