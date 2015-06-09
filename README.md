d82f941164565dc76202b29c28ea0ffbe3115183
cavalierejody <cavalierejody@yahoo.it>
2015-06-09 22:45:50 +0200

    added reorder PDF

--- README.md
+++ README.md
@@ -1,10 +1,15 @@
 # PDFutils
 
+## Requirements
+It requires pyPdf and pyPdf2 installed from pip.
+
 ## countPDFpages
 It counts the total number pages of the PDF-files present in a folder.
 
 ## pdfMerge
 It merges all pdf present in the folder.
 
-## Requirements
-It requires pyPdf and pyPdf2 installed from pip.
+## reorderPDF
+It reorders the pages acquired by a scanning process from 2sides documents in serie.
+### sample usage:
+./reorderPDF filename.pdf
---
+++ reorderPDF.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python2
+
+print
+print "Programma per l'acquisizione fronte/retro tramite scanner"
+print "                                   copyright Jody Cavalli"
+
+import sys
+import random
+
+from pyPdf import PdfFileWriter, PdfFileReader
+
+# read input pdf and instantiate output pdf
+output = PdfFileWriter()
+input1 = PdfFileReader(file(sys.argv[1],"rb"))
+
+# construct and shuffle page number list
+nPages = input1.getNumPages()
+print "n. pagine: " , nPages
+
+firstHalf = list(range(nPages/2))
+secondHalf = list(range(nPages-1,nPages/2-1,-1))
+
+#print "firstHalf: ", firstHalf
+#print "secondHalf", secondHalf
+
+pages = []
+
+for i in range(nPages/2):
+#    print i
+    pages.append(firstHalf[i])
+    pages.append(secondHalf[i])
+
+# display new sequence
+print "Riordinamento delle pagine secondo la successione:"
+print pages
+
+# add  the new sequence of pages to output pdf
+for page in pages:
+    output.addPage(input1.getPage(page))
+
+# write the output pdf to file
+outputStream = file(sys.argv[1]+'-fr.pdf','wb')
+output.write(outputStream)
+outputStream.close()
+
+print
